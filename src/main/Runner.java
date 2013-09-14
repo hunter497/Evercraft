fd6c41f64647aa5c44e059a8a7910d2de2381e1a
hunter497 <hunter.497@gmail.com>
2013-09-14 11:47:28 -0400

    Fixed the Runner.java class, it was utilizing Character instead of PlayerCharacter.

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -2,7 +2,7 @@ package Evercraft;
 
 public class Runner {
 	public static void main(String[] args) {
-		Character character = new Character();
+		PlayerCharacter character = new PlayerCharacter();
 		character.setName("Newbie");
 		System.out.println("The name of your character is: " +
 			character.getName());
